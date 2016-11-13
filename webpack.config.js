74484f782ec5503956ca7fbda8d477092d8cb1bf
Owen Trueblood <jmptable@mit.edu>
2016-11-13 13:13:42 -0500

    Only ignore node_modules inside of this module

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = [
             loaders: [
                 {
                     test: /\.js$/,
-                    exclude: /(node_modules)/,
+                    exclude: /ragecode\/node_modules/,
                     loader: 'babel-loader',
                     query: {
                         presets: ['es2015'],
